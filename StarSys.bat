4ec8561ac78bff0da0ad26dd0e373773510ad116
Masked-Idiot <101909445+Masked-Idiot@users.noreply.github.com>
2022-05-23 19:51:35 -0400

    Delete StarSys.bat

--- StarSys.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-copy main.star C:\Users\liamc\Documents\Python_Projects\StarSys\Interpreter
-if exist *.png (copy *.png C:\Users\liamc\Documents\Python_Projects\StarSys\Interpreter)
-cd Interpreter\
-python main.py
