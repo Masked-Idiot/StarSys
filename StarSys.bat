820f011d094440407f8187af84816b62a7b00b75
Masked-Idiot <101909445+Masked-Idiot@users.noreply.github.com>
2022-05-28 11:02:23 -0400

    Update StarSys.bat

--- StarSys.bat
+++ StarSys.bat
@@ -1,6 +1,5 @@
 @echo off
 set file=%1
-if not %file% == *.star (exit)
 copy %file% C:\Users\liamc\Documents\Python_Projects\StarSys\Interpreter
 cd C:\Users\liamc\Documents\Python_Projects\StarSys\Interpreter\
 python main.py
