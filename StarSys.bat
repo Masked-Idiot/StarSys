c782d99165c5d2236d129120eb4140535297b7dd
Masked-Idiot <101909445+Masked-Idiot@users.noreply.github.com>
2022-05-28 12:24:40 -0400

    Update StarSys.bat

--- StarSys.bat
+++ StarSys.bat
@@ -1,6 +1,5 @@
 @echo off
 set file=%1
-cls
 copy %file% C:\Users\liamc\Documents\Python_Projects\StarSys\Interpreter
 cd C:\Users\liamc\Documents\Python_Projects\StarSys\Interpreter\
 python main.py
