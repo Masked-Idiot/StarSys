4f07f531d13d7ce2ce3c0183e00404e8f60bb7a1
Masked-Idiot <101909445+Masked-Idiot@users.noreply.github.com>
2022-05-28 09:06:47 -0400

    Update StarSys.bat

--- StarSys.bat
+++ StarSys.bat
@@ -1,6 +1,10 @@
 @echo off
-set /p file="Please type in the path to the StarSys file you would like to run"
+:start
+echo Please type in the path to the StarSys file you would like to run
+set /p file=
+if %file% == exit() (exit)
 copy %file% C:\Users\liamc\Documents\Python_Projects\StarSys\Interpreter
 cd C:\Users\liamc\Documents\Python_Projects\StarSys\Interpreter\
 python main.py
 cd C:\Users\liamc\
+goto start
