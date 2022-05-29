4fc9daa64e5aa59fb8200d1538eac74acc086425
Masked-Idiot <101909445+Masked-Idiot@users.noreply.github.com>
2022-05-29 10:27:05 -0400

    Update makeStar.bat

--- makeStar.bat
+++ makeStar.bat
@@ -2,5 +2,6 @@
 cd C:\Users\liamc\Documents\Python_Projects\StarSys\Interpreter\
 pyinstaller --onefile app.py
 del app.spec
-dist\app.exe
+set /p dir=Please type in the path to the folder you would like to put the executable in
+copy dist\app.exe %dir%
 cd C:\Users\liamc\
