b1c8206da5b1c601b2bd77bd400d3c555d099df3
Masked-Idiot <101909445+Masked-Idiot@users.noreply.github.com>
2022-05-31 16:52:49 -0400

    Delete tranStar.bat

--- tranStar.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set file=%1
-copy %file% C:\Users\liamc\Documents\Python_Projects\StarSys\Interpreter
-cd C:\Users\liamc\Documents\Python_Projects\StarSys\Interpreter\
-python transpile.py
-cd C:\Users\liamc\
