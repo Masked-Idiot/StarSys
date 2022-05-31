400702638285f0d5068744b29acd23528de65090
Masked-Idiot <101909445+Masked-Idiot@users.noreply.github.com>
2022-05-31 16:52:57 -0400

    Delete makeStar.bat

--- makeStar.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set file=%1
-transtar %file%
-set dir=%2
-cd C:\Users\liamc\Documents\Python_Projects\StarSys\Interpreter\
-pyinstaller --onefile app.py
-del app.spec
-copy dist\app.exe %dir%
-cd C:\Users\liamc\
