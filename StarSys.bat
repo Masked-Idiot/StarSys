2d74efb0e14e9969e33b25745205477285d35195
Masked-Idiot <101909445+Masked-Idiot@users.noreply.github.com>
2022-05-31 16:58:32 -0400

    Add files via upload

---
+++ StarSys.bat
@@ -0,0 +1,6 @@
+@echo off
+set file=%1
+copy %file% C:\Users\liamc\Documents\Python_Projects\StarSys\Interpreter
+cd C:\Users\liamc\Documents\Python_Projects\StarSys\Interpreter\
+python main.py
+cd C:\Users\liamc\
